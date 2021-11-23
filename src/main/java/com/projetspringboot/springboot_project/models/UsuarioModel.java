736dd15c66e80e72d9c30ab1607e255b1346bc1b
Roxor <junyperez97@gmail.com>
2021-11-23 09:24:50 -0500

    se documento la clase UsuarioModel

--- src/main/java/com/projetspringboot/springboot_project/models/UsuarioModel.java
+++ src/main/java/com/projetspringboot/springboot_project/models/UsuarioModel.java
@@ -8,9 +8,11 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+//creo una tabla en la base de datos de nombre usuario
 @Table(name = "usuario")
 public class UsuarioModel {
 
+    // creo variables para crear el id,el nombre y la prioridad en la tabla.
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true, nullable = false)
@@ -18,30 +20,37 @@ public class UsuarioModel {
     private String nombre;
     private String email;
     private Integer prioridad;
+
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public Integer getPrioridad() {
         return prioridad;
     }
+
     public void setPrioridad(Integer prioridad) {
         this.prioridad = prioridad;
     }
-    
-    
+
 }

5041defe9cd1da84285d67333b8ef864a0c85c88
Roxor <junyperez97@gmail.com>
2021-11-23 09:29:58 -0500

    se refactorizo y documento la clase UsuarioController

--- src/main/java/com/projetspringboot/springboot_project/controllers/UsuarioController.java
+++ src/main/java/com/projetspringboot/springboot_project/controllers/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.projetspringboot.springboot_project.controllers;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import com.projetspringboot.springboot_project.models.UsuarioModel;
@@ -16,32 +17,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//clase que se encarga de manejar las peticiones de los usuarios
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
     @Autowired
     UsuarioService usuarioService;
 
+    //metodo para obtener todos los usuarios
     @GetMapping()
-    public ArrayList<UsuarioModel> obtenerUsuarios() {
+    public List<UsuarioModel> obtenerUsuarios() {
         return usuarioService.obtenerUsuarios();
     }
 
+    //metodo para guardar y actualizar un usuario por su id
     @PostMapping()
     public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario) {
         return this.usuarioService.guardarUsuario(usuario);
     }
 
+    //metodo para obtener un usuario por su id
     @GetMapping(path = "/{id}")
     public Optional<UsuarioModel> obtenerUsuario(@PathVariable("id") Long id) {
         return this.usuarioService.obtenerUsuario(id);
     }
 
+    //metodo para optener ususarios por prioridad
     @GetMapping("/query")
-    public ArrayList<UsuarioModel> obtenerUsuarioPorPrioridad(@RequestParam("prioridad") Integer prioridad) {
+    public List<UsuarioModel> obtenerUsuarioPorPrioridad(@RequestParam("prioridad") Integer prioridad) {
         return this.usuarioService.obtenerUsuarioPorPrioridad(prioridad);
     }
 
+    //metodo para eliminar un usuario por su id
     @DeleteMapping(path = "/{id}")
     public String eliminarUsuario(@PathVariable("id") Long id) {
         boolean ok = this.usuarioService.eliminarUsuario(id);

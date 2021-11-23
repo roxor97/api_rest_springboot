17d0b96e11c0300520d7da85c12975258c30afec
Roxor <junyperez97@gmail.com>
2021-11-23 09:21:09 -0500

    se refactorizo y documento la clase UsuarioService

--- src/main/java/com/projetspringboot/springboot_project/services/UsuarioService.java
+++ src/main/java/com/projetspringboot/springboot_project/services/UsuarioService.java
@@ -16,28 +16,29 @@ public class UsuarioService {
     @Autowired
     UsuarioRespository usuarioRespository;
 
-    
+    //creo un metodo para obtener todos los usuarios
     public List<UsuarioModel> obtenerUsuarios() {
 
         return (ArrayList<UsuarioModel>) usuarioRespository.findAll();
 
     }
 
-    
+    //creo un metodo para obtener un usuario por su id
     public UsuarioModel guardarUsuario(UsuarioModel usuario) {
         return usuarioRespository.save(usuario);
     }
 
-    
+    //creo un metodo para obtener un usuario por su id
     public Optional<UsuarioModel> obtenerUsuario(Long id) {
         return usuarioRespository.findById(id);
     }
 
-    
+    //creo un metodo para buscar usuarios por su prioridad
     public List<UsuarioModel> obtenerUsuarioPorPrioridad(Integer prioridad) {
         return usuarioRespository.findByPrioridad(prioridad);
     }
 
+    //creo un metodo para eliminar un usuario por su id
     public boolean eliminarUsuario(Long id) {
         try {
             usuarioRespository.deleteById(id);

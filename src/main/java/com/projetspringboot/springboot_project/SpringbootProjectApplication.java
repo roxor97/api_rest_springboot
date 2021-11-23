3996e7111cc3f9dd4f2b402c6b8edec2fad24f30
Roxor <junyperez97@gmail.com>
2021-11-23 10:15:58 -0500

    se empaqueto correctamente el proyecto

--- src/main/java/com/projetspringboot/springboot_project/SpringbootProjectApplication.java
+++ src/main/java/com/projetspringboot/springboot_project/SpringbootProjectApplication.java
@@ -1,10 +1,17 @@
 package com.projetspringboot.springboot_project;
 
+import com.fasterxml.jackson.databind.deser.impl.ExternalTypeHandler.Builder;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class SpringbootProjectApplication {
+public class SpringbootProjectApplication extends ServletInitializer {
+        @Override
+        public SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+                return application.sources(SpringbootProjectApplication.class);
+        }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootProjectApplication.class, args);
